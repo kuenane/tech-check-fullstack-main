a1a3fe748b2606730c3a9901b217274381ceb547
Lebohang Kuenane <lebohang.kuenane44@gmail.com>
2022-10-05 15:26:07 -0700

    Update CommentAnalyzer.java
    Changed comment initializer to accommodate concurrency

--- src/com/ikhokha/techcheck/CommentAnalyzer.java
+++ src/com/ikhokha/techcheck/CommentAnalyzer.java
@@ -1,4 +1,5 @@
 package com.ikhokha.techcheck;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -8,109 +9,93 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.ConcurrentMap;
+import java.util.concurrent.Callable;
 import java.util.regex.Pattern;
 
-public class CommentAnalyzer implements Runnable {
+public class CommentAnalyzer implements Callable<Map<String, Integer>>{
+	
+	private File file;
+	private List<String> terms;
 
-  // Used to check url on string
-  private static final String URL_REGEX =
-      "((http|https)://)(www.)?[a-zA-Z0-9@:%._\\+~#?&//=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%._\\+~#?&//=]*)";
+	@Override
+	public Map<String, Integer> call() {
+		return this.analyze();
+	}
+	
+	public CommentAnalyzer(File file, String terms) {
+		this.file = file;
+		this.terms = new ArrayList<String>();
+		this.addKeywords(terms);
+	}
+	
+	public Map<String, Integer> analyze() {
 
-  // Metrics list for reporting
-  final static List<CommentQuestion> commentQuestionList = initializeMetrics();
+		Map<String, Integer> resultsMap = new HashMap<>();
+		
+		try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+			
+			String line = null;
+			while ((line = reader.readLine()) != null) {
 
-  private File file;
+				line = line.toLowerCase();
 
-  private ConcurrentMap<String, Integer> totalResults;
+				if (line.length() < 15) {
+					incOccurrence(resultsMap, "SHORTER_THAN_15");
+				}
 
-  public CommentAnalyzer(final File file, final ConcurrentMap<String, Integer> totalResults) {
-    this.file = file;
-    this.totalResults = totalResults;
-  }
+				if (line.contains("?")) {
+					incOccurrence(resultsMap, "QUESTIONS");
+				}
 
-  @Override
-  public void run() {
+				Pattern urlPattern = Pattern.compile(
+					"\\b(https?|ftp|file)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]",
+					Pattern.CASE_INSENSITIVE);
+				if (urlPattern.matcher(line).find()) {
+					incOccurrence(resultsMap, "SPAM");
+				}
 
-    final Map<String, Integer> resultsMap = new HashMap<>();
+				for (String term : this.terms) {
+					if (line.contains(term)) {
+						incOccurrence(resultsMap, String.format("%s_MENTIONS", term.toUpperCase()));
+					}
+				}
+			}
+			
+		} catch (FileNotFoundException e) {
+			System.out.println("File not found: " + file.getAbsolutePath());
+			e.printStackTrace();
+		} catch (IOException e) {
+			System.out.println("IO Error processing file: " + file.getAbsolutePath());
+			e.printStackTrace();
+		}
 
-    try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+		return resultsMap;
+		
+	}
+	
+	/**
+	 * This method increments a counter by 1 for a match type on the countMap. Uninitialized keys will be set to 1
+	 * @param countMap the map that keeps track of counts
+	 * @param key the key for the value to increment
+	 */
+	private void incOccurrence(Map<String, Integer> countMap, String key) {
+		countMap.putIfAbsent(key, 0);
+		countMap.put(key, countMap.get(key) + 1);
+	}
 
-      String line = null;
-      while ((line = reader.readLine()) != null) {
-
-        for (CommentQuestion commentQuestion : commentQuestionList) {
-          if (commentQuestion.getValue().test(line)) {
-            incOccurrence(resultsMap, commentQuestion.getKey());
-          }
-        }
-      }
-
-    } catch (FileNotFoundException e) {
-      System.out.println("File not found: " + file.getAbsolutePath());
-      e.printStackTrace();
-    } catch (IOException e) {
-      System.out.println("IO Error processing file: " + file.getAbsolutePath());
-      e.printStackTrace();
-    }
-
-    // add count results into total results sequential
-    synchronized (totalResults) {
-      addReportResults(resultsMap, totalResults);
-    }
-  }
-
-  /**
-   * Initialize metrics for creating comment reports This method can be used to read metrics from
-   * data source
-   * 
-   * @return List of metrics
-   */
-  private static List<CommentQuestion> initializeMetrics() {
-    List<CommentQuestion> commentQuestions = new ArrayList<CommentQuestion>();
-
-    commentQuestions.add(new CommentQuestion("SHORTER_THAN_15").value(line -> line.length() < 15));
-    commentQuestions.add(
-        new CommentQuestion("MOVER_MENTIONS").value(line -> line.toLowerCase().contains("mover")));
-    commentQuestions.add(new CommentQuestion("SHAKER_MENTIONS")
-        .value(line -> line.toLowerCase().contains("shaker")));
-    commentQuestions.add(new CommentQuestion("QUESTIONS").value(line -> line.contains("?")));
-    commentQuestions.add(
-        new CommentQuestion("SPAM").value(line -> Pattern.compile(URL_REGEX).matcher(line).find()));
-
-    return commentQuestions;
-  }
-
-  /**
-   * This method adds the result counts from a source map to the target map
-   * 
-   * @param source the source map
-   * @param target the target map
-   */
-  private static void addReportResults(Map<String, Integer> source, Map<String, Integer> target) {
-
-    for (Map.Entry<String, Integer> entry : source.entrySet()) {
-      target.putIfAbsent(entry.getKey(), 0);
-      target.put(entry.getKey(), target.get(entry.getKey()) + entry.getValue());
-    }
-
-  }
-
-  /**
-   * This method increments a counter by 1 for a match type on the countMap. Uninitialized keys will
-   * be set to 1
-   * 
-   * @param countMap the map that keeps track of counts
-   * @param key the key for the value to increment
-   */
-  private void incOccurrence(Map<String, Integer> countMap, String key) {
-
-    countMap.putIfAbsent(key, 0);
-    countMap.put(key, countMap.get(key) + 1);
-  }
-
-public Map<String, Integer> analyze() {
-    return null;
-}
+	/**
+	 * This method allows us to add more keywords to filter in our report
+	 * @param terms string of terms split by a ","
+	 */
+	public List<String> addKeywords(String terms) {
+		String[] termsArr = terms.split(",");
+		for (String newTerm : termsArr) {
+			newTerm = newTerm.toLowerCase();
+			if(!this.terms.contains(newTerm)) {
+				this.terms.add(newTerm);
+			}
+		}
+		return this.terms;
+	}
 
 }
